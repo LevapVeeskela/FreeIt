ca4ba7c036e42077099499e718620d051342cd26
paulalexeev10@gmail.com <paulalexeev10@gmail.com>
2020-07-05 03:32:46 +0300

    +initial infrastructure added to the project for home tasks

---
+++ FreeIt.LowLevel.SecondWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeIt.LowLevel.SecondWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FreeIt.LowLevel.ThirdWeek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FreeIt.LowLevel.ThirdWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
