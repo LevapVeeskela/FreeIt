09676f90776b6cbb61a2c5591c0bba5f5dffb830
paulalexeev10@gmail.com <paulalexeev10@gmail.com>
2020-07-07 13:41:44 +0300

    +IoC added to the project

---
+++ FreeIt.Dependency/Configuration/IoC/DependencyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace FreeIt.Dependency.Configuration.IoC
+{
+    public static class DependencyConfiguration
+    {
+        public static IServiceCollection ConfigureDependencyServices(
+            this IServiceCollection services,
+            IHostingEnvironment environment,
+            IConfiguration configuration)
+            => services
+                .RegisterServices();
+    }
+}
