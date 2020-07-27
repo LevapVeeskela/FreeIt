a9c4b722521ebb4dcf5e1e0be25d9a04917dd31a
paulalexeev10@gmail.com <paulalexeev10@gmail.com>
2020-07-27 03:28:14 +0300

    + extarnal service for game millionaire created

---
+++ FreeIt.Domain.Interfaces/Services/External/IQueryBuilder.cs
@@ -0,0 +1,10 @@
+﻿using FreeIt.Domain.Common.Models.Requests.Millionaire;
+
+namespace FreeIt.Domain.Interfaces.Services.External
+{
+    public interface IQueryBuilder
+    {
+        string BuildQueryString(GetQuestionRequest request);
+
+    }
+}
---
+++ FreeIt.Domain.Services/LowLevel.FourthWeek/External/MillionaireClient.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using FreeIt.Domain.Common.Models.Requests.Millionaire;
+using FreeIt.Domain.Common.Models.Responses.Millionaire;
+using FreeIt.Domain.Interfaces.Services.External;
+using Marvin.StreamExtensions;
+
+namespace FreeIt.Domain.Services.LowLevel.FourthWeek.External
+{
+    public class MillionaireClient : IMillionaireClient
+    {
+        private const string ApiKey = "&apikey=";
+
+        private static readonly HttpClient Client = new HttpClient();
+
+        private readonly IQueryBuilder _queryBuilder;
+
+        public MillionaireClient(IQueryBuilder queryBuilder)
+            => _queryBuilder = queryBuilder;
+        
+        public async Task<MillionaireResponse> GetAsync(GetQuestionRequest request, CancellationToken token)
+        {
+            var queryString = _queryBuilder.BuildQueryString(request);
+
+            if (string.IsNullOrEmpty(request.ApiKey))
+                queryString = queryString.Replace(ApiKey, string.Empty);
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, queryString);
+
+            using (var httpResponse = await Client.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead, token))
+            {
+                httpResponse.EnsureSuccessStatusCode();
+                var stream = await httpResponse.Content.ReadAsStreamAsync();
+                return stream.ReadAndDeserializeFromJson<MillionaireResponse>();
+            }
+        }
+    }
+}
